fc7bcb267ad1a284660e34affc0f0d64a0c1195e
JuanRodriguezL <juangrl2143@gmail.com>
2022-11-08 08:45:30 -0500

    Implementacion para que los usuarios puedan ver y prestar libros disponibles

--- app/src/main/java/com/example/agendakotlin/UsuLibrosDisponibles.kt
+++ app/src/main/java/com/example/agendakotlin/UsuLibrosDisponibles.kt
@@ -1,11 +1,44 @@
 package com.example.agendakotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.agendakotlin.databinding.ActivityUsuLibrosDisponiblesBinding
 
-class UsuLibrosDisponibles : AppCompatActivity() {
+class UsuLibrosDisponibles : AppCompatActivity(), OnClickListener {
+
+    lateinit var lAdapter: DisponiblesAdapter
+    private lateinit var layout: GridLayoutManager
+    lateinit var usuLibrosBinding: ActivityUsuLibrosDisponiblesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        usuLibrosBinding = ActivityUsuLibrosDisponiblesBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_usu_libros_disponibles)
+        setContentView(usuLibrosBinding.root)
+        setupRecicler()
+    }
+
+    private fun setupRecicler() {
+        lAdapter = DisponiblesAdapter(mutableListOf(), this)
+        layout = GridLayoutManager(this, 2)
+        getLibrosDis()
+        usuLibrosBinding.reciclerLibrosDisponiblesUsu.apply {
+            setHasFixedSize(true)
+            layoutManager = layout
+            adapter = lAdapter
+        }
+    }
+
+    private fun getLibrosDis() {
+        val libro = ContactosAplication.dataBase.contactosDao().getAllLibros()
+        lAdapter.setLibros(libro)
+    }
+
+    override fun onClick(librosEntity: LibrosEntity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDelete(librosEntity: LibrosEntity) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/agendakotlin/UsuMisLibros.kt
+++ app/src/main/java/com/example/agendakotlin/UsuMisLibros.kt
@@ -1,5 +1,6 @@
 package com.example.agendakotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.agendakotlin.databinding.ActivityUsuMisLibrosBinding
@@ -11,5 +12,11 @@ class UsuMisLibros : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         usuBindig = ActivityUsuMisLibrosBinding.inflate(layoutInflater)
         setContentView(usuBindig.root)
+
+        usuBindig.buttonPrestar.setOnClickListener {
+            val intent = Intent(this, UsuLibrosDisponibles::class.java)
+            startActivity(intent)
+        }
     }
+
 }
